2ff1479470fe473e1dceae8267c478865f26d50c
UnderpaidDev1 <UnderpaidDev1@gmail.com>
2021-01-21 17:02:50 -0500

    updates player health and mana on exit

--- wizsdk/battle.py
+++ wizsdk/battle.py
@@ -10,22 +10,48 @@ from .pixels import DeviceContext, match_image
 from .card import Card
 from .utils import packaged_img
 
-# TODO
-
 
 class Battle(DeviceContext):
+    """
+    Battle class
+    
+    Example:
+        .. code-block:: py
+        
+            # register the client
+            p1 = Client.register(name="Bot")
+            # Get a battle object from the client
+            battle = p1.get_battle("Test")
+            # Loop
+            while await battle.loop():
+                if battle.round_count == 1:
+                    # pass on the first round
+                    p1.pass_turn()
+                else:
+                    # Cast enchanted tempest
+                    temp = await p1.find_spell("tempest")
+                    epic = await p1.find_spell("epic")
+
+                    if temp and epic:
+                        e_temp = await epic.enchant(temp)
+                        await e_temp.cast()
+                    else:
+                        await p1.pass_turn()
+    """
+
     def __init__(self, client, name=None):
         super().__init__(client.window_handle)
-        self.round_count = 1
         self.client = client
         self.logging = self.client.logging
         self.is_idle = self.client.is_idle
         self.name = name
 
-        self.going_first = False
+        self._round_count = 1
+        self._going_first = False
         self.enemy_first = False
 
         self.is_over = False
+
         self.in_progress = False
 
         # rectangles defined as (x, y, width, height)
@@ -33,15 +59,38 @@ class Battle(DeviceContext):
         self._enemy_area = (68, 26, 650, 50)
         self._ally_area = (140, 580, 650, 50)
 
+    @property
+    def round_count(self) -> int:
+        """
+        Current round of the fight. Starts at 1
+        """
+        return self._round_count
+
+    @property
+    def going_first(self) -> bool:
+        """
+        True if your team is going first, False if enemies are going first. Value is set once at the beginning of the fight.
+        """
+        return self._going_first
+
     async def loop(self):
         """
-        Takes care of the looping logic for a battle
+        Handles the looping logic for a battle. The loops exists when the battle ends.
+        
+        Examples:
+            .. code-block:: py
+                
+                while await battle.loop():
+                    print(battle.round_count)
+                    print(battle.enemy_first)
+                    print(battle.get_enemy_count())
         """
+
         if not self.in_progress and not self.is_over:
-            await self.start()
+            await self._start()
             return True
         else:
-            await self.next_turn()
+            await self._next_turn()
             return not self.is_over
 
     def log(self, message):
@@ -54,9 +103,9 @@ class Battle(DeviceContext):
             sys.stdout.flush()
 
     def print_round(self):
-        self.log(f"----------- Battle round {self.round_count} -----------")
+        self.log(f"----------- Battle round {self._round_count} -----------")
 
-    def is_turn(self) -> bool:
+    def _is_turn(self) -> bool:
         """
         Returns if it's our turn to play
         by matching pixels in the `flee` button
@@ -65,21 +114,21 @@ class Battle(DeviceContext):
         flee_brown = self.pixel_matches_color((578, 394), (124, 68, 0), tolerance=30)
         return flee_yellow and flee_brown
 
-    async def start(self) -> None:
+    async def _start(self) -> None:
         """
         Waits for the first round then signals to the class that the battle has started.
         used in the `loop()` method
         """
-        while not self.is_turn():
+        while not self._is_turn():
             await asyncio.sleep(1)
 
         self.is_over = False
         self.in_progress = True
 
-        self.enemy_first = self.is_enemy_first()
-        self.going_first = not self.enemy_first
+        self.enemy_first = self._is_enemy_first()
+        self._going_first = not self.enemy_first
 
-        self.log("Battle is starting")
+        self.log("Battle is _starting")
         if self.enemy_first:
             self.log("Enemy is going first")
         else:
@@ -87,25 +136,32 @@ class Battle(DeviceContext):
 
         self.print_round()
 
-    async def next_turn(self) -> None:
+    async def _next_turn(self) -> None:
         """
         Sleeps until next round or fight has ended
         Sets `is_over` to True if the fight has ended
-        Increase the `round_count` otherwise
+        Increase the `_round_count` otherwise
         Used in the `loop()` method
         """
-        while self.is_turn():
+        while self._is_turn():
             await asyncio.sleep(1)
 
-        while not self.is_turn() and not self.is_idle():
+        while not self._is_turn() and not self.is_idle():
             await asyncio.sleep(1)
 
         if self.is_idle():
             self.log("Battle has finished")
+
+            # Update player health and mana
+            if p1.walker._memory.is_hook_active("player_struct"):
+                await self.client.get_mana()
+                await self.client.get_health()
+
+            await asyncio.sleep(0.5)
             self.is_over = True
             self.in_progress = False
         else:
-            self.round_count += 1
+            self._round_count += 1
             self.print_round()
 
     def get_enemy_positions(self):
@@ -159,11 +215,11 @@ class Battle(DeviceContext):
 
         return False
 
-    def is_enemy_first(self):
+    def _is_enemy_first(self):
         turn_arrow_region = (230, 240, 80, 60)
         return bool(
             self.client.locate_on_screen(
-                packaged_img("enemy-first.png"), region=turn_arrow_region
+                packaged_img("enemy-first.png"), region=turn_arrow_region, threshold=0.2
             )
         )
 
